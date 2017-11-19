51ed05470b4adc9f7034407d8d5cd0e2714db3bf
Piotr Butkiewicz <piotrab@gmail.com>
2017-11-19 17:33:14 -0500

    split Book into it's own component and move chelf movement function into the Book Component

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
+
+  moveBook(book, event) {
+    if(event) {
+      const newShelf = event.target.value
+      if (newShelf !== 'none') {
+        this.props.handleChange(book, newShelf)
+      }
+    }
+  }
+
   render() {
 
     const { book } = this.props
@@ -10,7 +20,10 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select 
+              onChange={this.moveBook.bind(this, book)}
+              defaultValue={book.shelf}  
+            >
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class Bookshelf extends Component {
   
-  moveBook(book, event) {
-    if(event) {
-      const newShelf = event.target.value
-      if (newShelf !== 'none') {
-        this.props.handleChange(book, newShelf)
-      }
-    }
-  }
   render() {
-    const { shelf } = this.props
+    const { shelf, handleChange } = this.props
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{shelf.title}</h2>
@@ -19,25 +12,7 @@ class Bookshelf extends Component {
           <ol className="books-grid">
             {shelf.books.map((book) => (
               <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
-                    <div className="book-shelf-changer">
-                      <select 
-                        onChange={this.moveBook.bind(this, book)}
-                        defaultValue={book.shelf}  
-                      >
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.author}</div>
-                </div>
+                <Book book={book} handleChange={handleChange}/>
               </li>
             ))}
           </ol>
