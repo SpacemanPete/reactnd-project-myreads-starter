4aa49a877b842ef455d35333b4c736a425f43c9e
Piotr Butkiewicz <piotrab@gmail.com>
2017-11-21 20:16:01 -0500

    SearchBooks draft - needs work

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class SearchBooks extends Component {
+
+  state = {
+    query: '',
+    foundBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+    if (query) {
+      BooksAPI.search(query.trim(), 10).then( (foundBooks) => (
+        this.setState({ foundBooks })
+      ))
+    }
+  }
+
   render () {
+    const { query, foundBooks } = this.state
     return (
       <div className="search-books">
       <div className="search-books-bar">
         <Link to="/" className="close-search">Close</Link>
         <div className="search-books-input-wrapper">
-          {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
-
+          <input 
+            type="text" 
+            placeholder="Search by title or author"
+            value={query}
+            onChange={ (event) => this.updateQuery(event.target.value) }
+          />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {foundBooks.map( (book) => (
+            <li key={book.id}>
+            <Book book={book}/>
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
     )
