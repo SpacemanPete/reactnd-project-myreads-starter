56d90a07f85d6579ac97c7690588fefea4435991
Piotr Butkiewicz <piotrab@gmail.com>
2017-11-23 19:33:07 -0500

    refactor Bookshelf into stateless Functional Component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Book from './Book'
 
-class Bookshelf extends Component {
+const Bookshelf = ({ title, shelfFilter, books, handleChange }) => {
+
+  const bookList = books.filter((book) => {
+    return book.shelf === shelfFilter
+  })
+  console.log(shelfFilter);
   
-  render() {
-    const { title, shelfFilter, books, handleChange } = this.props
-    const bookList = books.filter((book) => {
-      return book.shelf === shelfFilter
-    })
-    console.log(shelfFilter);
-    
-    console.log(bookList);
-    
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {bookList.map((book) => (
-              <li key={book.id}>
-                <Book 
-                  book={book} 
-                  handleChange={handleChange}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+  console.log(bookList);
+  
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {bookList.map((book) => (
+            <li key={book.id}>
+              <Book 
+                book={book} 
+                handleChange={handleChange}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Bookshelf.PropTypes = {
+  title: PropTypes.string.isRequired,
+  shelfFilter: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  handleChange: PropTypes.func.isRequired
+
 }
 
 export default Bookshelf
